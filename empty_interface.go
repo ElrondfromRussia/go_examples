2284d97bfe4ba1c2af0cdd334797b43059d550bc
Margaret <48628275+ElrondfromRussia@users.noreply.github.com>
2020-01-05 19:29:43 +0300

    Create empty_interface.go

---
+++ empty_interface.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type Payer interface {
+	Pay(int) error
+}
+//------------------
+type Wallet struct {
+	Cash int
+}
+func (w *Wallet) Pay(amount int) error {
+	if w.Cash < amount {
+		return fmt.Errorf("Не хватает денег в кошельке")
+	}
+	w.Cash -= amount
+	return nil
+}
+
+//-----------------
+func (w *Wallet) String() string {
+	return "Кошелёк в котором " + strconv.Itoa(w.Cash) + " денег"
+}
+
+//---------------
+func Buy(in interface{}) {
+	var p Payer
+	var ok bool
+	if p, ok = in.(Payer); !ok {
+		fmt.Printf("%T не не является платежным средством\n\n", in)
+		return
+	}
+	err := p.Pay(10)
+	if err != nil {
+		fmt.Printf("Ошибка при оплате %T: %v\n\n", p, err)
+		return
+	}
+	fmt.Printf("Спасибо за покупку через %T\n\n", p)
+}
+
+//----------------
+func main() {
+	myWallet := &Wallet{Cash: 100}
+	//fmt.Printf("Raw payment : %#v\n", myWallet)
+	//fmt.Printf("Способ оплаты: %s\n", myWallet)
+	Buy(myWallet)
+	Buy([]int{1, 2, 3})
+	Buy(3.14)
+	return
+}
+
+
